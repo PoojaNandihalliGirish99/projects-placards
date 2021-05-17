da9ca2054148f29c408d2f7f761a3170dfc28ba1
Pooja N G <PoojaNandihalliGirish99@users.noreply.github.com>
2021-05-17 10:36:49 +0530

    actions, reducers, combineReducers

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,14 @@
+
+const initState = {
+    projects:[
+        {id:'1', title:'hello there', content:'blah sxdkjenskc'},
+        {id:'2', title:'hello sdddde', content:'blah sxdkjenskc'},
+        {id:'3', title:'hello thawwawaeglerdfcde', content:'blah sxdkjenskc'},
+    ]
+}
+
+const projectReducer = (state = initState, action) => {
+    return state
+}
+
+export default projectReducer;
